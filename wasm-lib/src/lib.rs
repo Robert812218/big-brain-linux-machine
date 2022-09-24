d579d1d7f6002910fbf0268e7d93537d02b5258b
Robert Kelly <robert812@shaw.ca>
2022-09-24 17:12:52 -0600

    more updates to structs

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -7,27 +7,27 @@ pub fn add(a: i32, b: i32) -> i32 {
 
 #[test]
 pub fn add_test() {
-    assert_eq!(5 + 5, add(5, 5));
+    assert_eq!(5 + 5, add(5, 5)),
 }
 
 /////////
 //#[wasm-bindgen]
 //pub mod cat {
 //    pub fn meow() {
-//        crate::dog::woof();
+//        crate::dog::woof(),
 //    }
 //}
 //
 //#[wasm-bindgen]
 //pub mod dog {
 //    pub fn woof() {
-//        println!("Woof Woof!");
+//        println!("Woof Woof!"),
 //    }
 //}
 //
 //#[wasm-bindgen]
 //pub fn test_one() {
-//    cat::meow();
+//    cat::meow(),
 //}
 ////////////
 
@@ -35,11 +35,14 @@ pub fn add_test() {
 pub mod command_list() {
     pub struct save_and_exit {
          // :e myfile - Opens "myfile" for editing
-         open_myfile: String = ":e",
+         // open_myfile: String = ":e",
+         0: ("Opens 'myfile' for editing".to_string(), ":e".to_string()),
          // :w - Save file
-         save_file: String = ":w",
+         // save_file: String = ":w",
+         1: ("Save file".to_string(), ":w".to_string()),
          // :sav myfile.txt
-         save_myfile_txt: String = ":sav myfile.txt",
+         // save_myfile_txt: String = ":sav myfile.txt",
+         
          // 😡 - Write changes to file and exit
          // ^ Not sure how to implement emojis rn, will deal with that later
          // :q! - Quit without saving changes
@@ -51,93 +54,128 @@ pub mod command_list() {
 
     pub struct find_and_replace() {
         // y    Copy the selected text
-        copy_selected_text: String = "y";
+        copy_selected_text: String = "y",
         // p    Paste the selected text
-        paste_selected_text: String = "p";
+        paste_selected_text: String = "p",
         // dd   Cut the current line
-        cut_current_time: String = "dd";
+        cut_current_time: String = "dd",
         // yy   Copy the current line
-        copy_current_line: String = "yy";
+        copy_current_line: String = "yy",
         // y$   Copy to EOL
-        copy_to_eol: String = "y$";
+        copy_to_eol: String = "y$",
         // D    Cut to EOL
-        cut_to_eol: String = "D";
+        cut_to_eol: String = "D",
 
     }
     
     pub struct change_case_commands() {
         // Vu   Lowercase the entire line
-        lowercase_entire_line: string = "Vu";
+        lowercase_entire_line: String = "Vu",
+
         //VU    Uppercase the entire line
-        uppercase_entire_line: String = "VU";
+        uppercase_entire_line: String = "VU",
         //g~~   Invert selected case
-        invert_selected_line: String = "g~~";
+        invert_selected_line: String = "g~~",
         //vEU   Switch selected word to uppercase
-        switch_word_to_uppercase: String = "vEU";
+        switch_word_to_uppercase: String = "vEU",
         //vE~   Modify the current word case
-        modify_current_word_case: String = "vE~";
+        modify_current_word_case: String = "vE~",
         // ggguG Set all text to lowercase
-        set_all_text_lower: String = "ggguG";
+        set_all_text_lower: String = "ggguG",
         //gggUG Set all text to uppercase
-        set_all_text_upper: String = "gggUG";
+        set_all_text_upper: String = "gggUG",
         //:set ignorecase       Ignore case when searching text
-        set_ignorecase: String = ":set ignorecase";
+        set_ignorecase: String = ":set ignorecase",
         //:set smartcase        Ignore case in search, but not if an uppercase letter is used
-        set_smartcase: String = ":set smartcase";
+        set_smartcase: String = ":set smartcase",
         //:%s/\<./\u&/g Sets the first letter of each word to uppercase
-        first_letter_each_word_upper: String = ":%s/\<./\u&/g";
+        first_letter_each_word_upper: String = ":%s/\<./\u&/g",
         //:%s/\<./\l&/g Sets the first letter of each word to lowercase
-        first_letter_each_word_lower: String = "";
+        first_letter_each_word_lower: String = ":%s/\<./\l&/g",
         //:%s/.*/\u&    Sets the first letter of each line to uppercase
-        first_letter_each_line_uppercase: String ="";
+        first_letter_each_line_uppercase: String =":%s/.*/\u&",
         //:%s/.*/\l&    Sets the first letter of each line to lowercase
-        first_letter_each_line_lowercase: String = "";
+        first_letter_each_line_lowercase: String = ":%s/.*/\l&",
     }
 
     pub struct navigate_within_file() {
         // k or Up Arrow        Move the cursor position up one line
-        //j or Down Arrow       Move the cursor down one line
+        cursor_oneline_up: ("k", "[up arrow]"), 
+        //j or Down Arrow Move the cursor down one line
+        cursor_oneline_down: ("j", "[down arrow]"), 
         //e     Move the cursor to the end of the word
+        move_cursor_word_end: "e",
         //b     Move the cursor to the beginning of the word
+        move_cursor_word_begin: "b",
         //0     Move the cursor to the beginning of the line
+        move_cursor_line_begin: "0",
         //G     Move the cursor to EOF
+        move_cursor_EOF: "G",
         //gg    Move the cursor to the beginning of the file
+        move_cursor_file_begin: "gg",
         //L     Move the cursor to the bottom of the screen
+        move_cursor_bottom_screen: "L",
         //:80   Move the cursor to line number 80
+        move_cursor_line_80: ":80",
         //%     Move the cursor to matching parenthesis
+        move_cursor_matching_parenthesis: "%",
         //[[    Move the cursor to function start
+        move_cursor_function_start: "[[",
         //[{    Move the cursor to block start
+        move_cursor_function_start: "[{",
 
     }
 
     pub struct file_explorer_read_write() {
         // :1,10 w myfile       Saves lines 1 to 10 in myfile
+        save_lines_1to10: ":1,10 w myfile",
         //:1,10 w >> myfile     Appends lines 1 to 10 to myfile
+        append_lines_1to10: "1, 10w",
         //:r myfile     Inserts the content of myfile to current file
+        insert_myfile_to_current: ":r myfile",
         //:23r myfile   Inserts the content of myfile under line 23
+        insert_content_myfile_under_line_23: ":23r",
         //:e .  Open the File Explorer
+        open_file_explorer: ":e .",
         //:Sex  Split window and open File Explorer
+        split_window_file_explorer: ":Sex",
         //:Sex! Same as :Sex but splits window vertically
+        split_window_vertically: ":Sex!",
         //:browse e     Graphical File Explorer
+        graphical_file_explorer: ":browse e",
         //:ls   List buffers
+        list_buffers: ":ls",
         //:cd ..        Move to parent or root directory
+        move_to_parent_or_root_dir: ":cd",
         //:args List files
+        list_files: ":args",
         //:args *.php   Open file list with .php extension
+        open_file_list_with_php_extension: ":args *.php",
         //:grep something *.php Returns list of .php files containing something
+        return_php_file_list_containing_something: ":grep something *.php",
         //gf    Open file name under cursor
-
+        open_file_name_under_cursor: "gf",
     }
 
     pub struct interface_commands_tabs_windows() {
         // :tabnew      Opens a new tab
+        open_new_tab: ":tabnew",
         //gt    Go to next tab
+        go_to_next_tab: "gt",
         //:tabfirst     Go to first tab
+        go_to_first_tab: ":tabfirst",
         //:tablast      Go to last tab
+        go_to_last_tab: ":tablast",
         //:tabm n(position)     Rearrange open tabs
+        rearrange_open_tabs: ":tabm n(position)",
         //:tabdo %s/foo/bar/g   Execute same command in all tabs
+        execute_same_command_all_tabs: ":tabdo %s/foo/bar/g",
         //:tab ball     Puts all open files in different tabs
+        all_open_tabs_to_diff: ":tab ball",
         //:new myfile.txt       Edit myfile.txt in new window
+        edit_myfiletxt_in_new_window: ":new myfile.txt",
         //:e filename   Edit filename in current window
+        edit_filename_in_current_window: ":e",
         //:split myfile Split the window and open myfile
         //ctrl-w + Up arrow     Puts cursor in top window
         //ctrl-w ctrl-w (twice) Puts cursor in next window
@@ -189,5 +227,4 @@ pub mod command_list() {
     }
 }
 
-        
     
