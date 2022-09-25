600d4ee72f2acf56c92a16e55bc9d51f5b33aed1
Robert812218 <robert812@shaw.ca>
2022-09-24 19:45:40 -0600

    set up desktop

---
+++ wasm-lib/target/debug/deps/wasm_lib-1d93cd0e82acaf3a.d
@@ -0,0 +1,5 @@
+/home/bobdonald/coding/big-brain-linux-machine/wasm-lib/target/debug/deps/wasm_lib-1d93cd0e82acaf3a: src/lib.rs
+
+/home/bobdonald/coding/big-brain-linux-machine/wasm-lib/target/debug/deps/wasm_lib-1d93cd0e82acaf3a.d: src/lib.rs
+
+src/lib.rs:
--- wasm-lib/target/release/deps/wasm_bindgen_shared-0da7ca318225f717.d
+++ wasm-lib/target/release/deps/wasm_bindgen_shared-0da7ca318225f717.d
@@ -1,8 +1,8 @@
-/home/bobdonald/coding/react/big-brain-linux-machine/wasm-lib/target/release/deps/wasm_bindgen_shared-0da7ca318225f717.rmeta: /home/bobdonald/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/src/lib.rs
+/home/bobdonald/coding/big-brain-linux-machine/wasm-lib/target/release/deps/wasm_bindgen_shared-0da7ca318225f717.rmeta: /home/bobdonald/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/src/lib.rs
 
-/home/bobdonald/coding/react/big-brain-linux-machine/wasm-lib/target/release/deps/libwasm_bindgen_shared-0da7ca318225f717.rlib: /home/bobdonald/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/src/lib.rs
+/home/bobdonald/coding/big-brain-linux-machine/wasm-lib/target/release/deps/libwasm_bindgen_shared-0da7ca318225f717.rlib: /home/bobdonald/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/src/lib.rs
 
-/home/bobdonald/coding/react/big-brain-linux-machine/wasm-lib/target/release/deps/wasm_bindgen_shared-0da7ca318225f717.d: /home/bobdonald/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/src/lib.rs
+/home/bobdonald/coding/big-brain-linux-machine/wasm-lib/target/release/deps/wasm_bindgen_shared-0da7ca318225f717.d: /home/bobdonald/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/src/lib.rs
 
 /home/bobdonald/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/src/lib.rs:
 
